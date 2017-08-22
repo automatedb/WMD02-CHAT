faa8ca9eab8d88d21c197253882c08345a61d8f5
Iman <imangatine@gmail.com>
2017-08-22 16:53:24 +0200

    issue-#17-creation-model-user -suppression id et token

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,7 +1,5 @@
 var UserSchema = new Schema({
-    id   : ObjectId,
     login   : { type: String, required: true, unique : true },
-    pwd   : { type: String, required: true},
-    token   : String
+    pwd   : { type: String, required: true}
 });
 module.exports = mongoose.model('User', UserSchema);
