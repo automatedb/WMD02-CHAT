097151749241401b747c34c58b54fbaac324fed1
DUVAL <duval.laura.44@hotmail.fr>
2017-08-22 15:50:34 +0200

    issue-#40-Initialisation-Winston

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 const express = require('express');
 // import mongoose
 const mongoose = require('mongoose');
+// import winston
+const winston = require('winston');
+
+
 // instanciation d'express
 const app = express();
 
+// connexion à la BDD via le gramework MonGoose
 mongoose.createConnection('mongodb://138.197.113.194:27017/data_dev');
+
+
 // exemple méthode get
 app.get('/', function (req, res) {
     res.send('Hello World!');
