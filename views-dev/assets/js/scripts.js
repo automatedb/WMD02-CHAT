3b2e965c39def4848bb8964930bf84c41e4e9591
DUVAL <duval.laura.44@hotmail.fr>
2017-08-24 11:19:11 +0200

    issue #65 - Afficher Register depuis Login

--- views-dev/assets/js/scripts.js
+++ views-dev/assets/js/scripts.js
@@ -1,7 +1,11 @@
+// au chargement de la page on affiche le form de connexion
+$('main').load('/login.html');
+
 
 
 // à afficher quand l'utilisateur demande à créer un compte depuis le formulaire de connexion
-$('main').load('/register.html');
-
-$('main').load('/login.html');
+$('main').on('click', '#go-to-register', function(e){
+    e.preventDefault();
+    $('main').load('/register.html');
+});
 
